ef961fe1728b7def4984565c2db7e13179db977d
Jeremy Hocquard <89561398+flightlessnightbird@users.noreply.github.com>
2022-09-12 18:18:53 +1200

    store data for generator as arrays nested in object

--- horoscope-generator.js
+++ horoscope-generator.js
@@ -1,1 +1,7 @@
 console.log('Welcome to the Horoscope Generator')
+
+const signData = {
+    signX: ['aligned with Jupiter', 'channeling the cosmic rays from the outer universe', 'taking on excess energy right now', 'being drawn to an unknown source of dark matter', 'seeking Venus and Mercury', 'looking into the Sun\'s rays'],
+    signY: ['sheer misery', 'overwhelming joy', 'great fortune', 'great things to look forward to', 'a lot of deep sadness', 'pain and anxiety', 'amazing love'],
+    signZ: ['keep calm and carry on', 'look on the bright side', 'hide from the world for a while', 'invest in yourself', 'treat yoself', 'get back to your roots', 'have a break, have a Kitkat', 'find something to occupy your mind', 'pray for forgiveness'];
+}
